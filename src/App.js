e6ef1be0a59bad3ba6351fd9cb74818cccb3a202
loki <rus_basket_kg@inbox.ru>
2018-09-02 17:56:16 +0600

    Added data for each message, dynamic icon

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import ListView from './components/ListView/ListView';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        
-        <div className="parent-component">
-          <div className="child-component">
+    
+        <div className="Parent">
+          <div>
             <ListView/>
           </div>
         </div>
-        
-      </div>
+    
     );
   }
 }
--- src/components/ListView/ListView.js
+++ src/components/ListView/ListView.js
@@ -6,21 +6,21 @@ import loremIpsum from 'lorem-ipsum';
 import { List } from "react-virtualized";
 
 const rowCount = 1000;
-const listHeight = 600;
+const listHeight = 800;
 const rowHeight = 50;
-const rowWidth = 800;
+const rowWidth = 1000;
 
 class ListView extends Component {
   constructor() {
     super();
-    this.renderRow = this.renderRow.bind(this);
     this.list = Array(rowCount).fill().map((val, idx) => {
       return {
         id: idx, 
-        name: 'John Doe',
-        image: 'http://via.placeholder.com/40',
+        name: `Robot - ${idx}`,
+        image: `https://robohash.org/${idx}`,
+        date: new Date().toLocaleString(), 
         text: loremIpsum({
-          count: 1, 
+          count: 2, 
           units: 'sentences',
           sentenceLowerBound: 4,
           sentenceUpperBound: 8 
@@ -29,16 +29,17 @@ class ListView extends Component {
     });
   }
   
-  renderRow({ index, key, style }) {
+  renderRow = ({ index, key, style }) => {
     return (
       <div key={key} style={style} className="row">
         <div className="image">
-          <img src={this.list[index].image} alt="" />
+          <img src={this.list[index].image} alt="" className="icon"/>
         </div>
         <div className="content">
           <div>{this.list[index].name}</div>
           <div>{this.list[index].text}</div>
         </div>
+        <div className="date">{this.list[index].date}</div>
       </div>
     );
   }
