0a09178a53d3149c37ad3d5b10fa9868058eccd5
loki <rus_basket_kg@inbox.ru>
2018-09-03 00:50:41 +0600

    Added autoresizing and dynamic height rows

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import ListView from './components/ListView/ListView';
-import TextField from './components/TextField/TextField'
+
 class App extends Component {
   render() {
     return (
-    
-        <div className="Parent">
-          <div>
+        <div>
             <ListView/>
-          </div>
         </div>
-      
     );
   }
 }
--- src/components/ListView/ListView.js
+++ src/components/ListView/ListView.js
@@ -3,58 +3,76 @@ import './ListView.css';
 
 import loremIpsum from 'lorem-ipsum';
 
-import { List } from "react-virtualized";
+import { List, AutoSizer, CellMeasurer, CellMeasurerCache } from "react-virtualized";
 
 const rowCount = 1000;
-const listHeight = 800;
-const rowHeight = 50;
-const rowWidth = 1000;
 
 class ListView extends Component {
   constructor() {
     super();
+  
     this.list = Array(rowCount).fill().map((val, idx) => {
       return {
         id: idx, 
         name: `Robot - ${idx}`,
         image: `https://robohash.org/${idx}`,
-        date: new Date().toLocaleString(), 
+        date: new Date().toLocaleString(),
         text: loremIpsum({
           count: 2, 
           units: 'sentences',
-          sentenceLowerBound: 4,
-          sentenceUpperBound: 8 
+          sentenceLowerBound: 10,
+          sentenceUpperBound: 100 
         })
       }
     });
+    this.cache = new CellMeasurerCache({
+      fixedWidth: true,
+      defaultHeight: 100
+    });
   }
   
-  renderRow = ({ index, key, style }) => {
+  renderRow = ({ index, key, style, parent }) => {
     return (
-      <div key={key} style={style} className="row">
-        <div className="image">
-          <img src={this.list[index].image} alt="" className="icon"/>
-        </div>
-        <div className="content">
-          <div>{this.list[index].name}</div>
-          <div>{this.list[index].text}</div>
-        </div>
-        <div className="date">{this.list[index].date}</div>
-      </div>
+      <CellMeasurer 
+        key={key}
+        cache={this.cache}
+        parent={parent}
+        columnIndex={0}
+        rowIndex={index}>
+          <div style={style} className="row">
+            <div className="image">
+              <img src={this.list[index].image} alt="" className="icon"/>
+            </div>
+            <div className="content">
+              <div className="author">{this.list[index].name}</div>
+              <div>{this.list[index].text}</div>
+              <div className="date">{this.list[index].date}</div>
+            </div>
+          </div>
+      </CellMeasurer>
     );
   }
   
   render() {
     return (
+      <div className="App">
         <div className="list">
-          <List
-            width={rowWidth}
-            height={listHeight}
-            rowHeight={rowHeight}
-            rowRenderer={this.renderRow}
-            rowCount={this.list.length}
-            overscanRowCount={3} />
+          <AutoSizer>
+          {
+            ({ width, height }) => {
+              return <List
+                width={width}
+                height={height}
+                deferredMeasurementCache={this.cache}
+                rowHeight={this.cache.rowHeight}
+                rowRenderer={this.renderRow}
+                rowCount={this.list.length}
+                overscanRowCount={3} />
+            }
+          }
+          </AutoSizer>
         </div>
+      </div>
     );
   }
 }
